b1408dc3a987e8d3cf50aa3d77899ae8630b8c55
Raneem-Alshaikh <raneemalshekh98@gmail.com>
2021-02-12 12:58:52 +0200

    UI update

--- DTO/CreateTaskDto.cs
+++ DTO/CreateTaskDto.cs
@@ -1,4 +1,5 @@
 ﻿using FinalProject.Models;
+using FinalProject.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -13,11 +14,13 @@ namespace FinalProject.DTO
         [Required]
         public string Title { get; set; }
         [Required]
-        
+        [CheckPastDate(ErrorMessage = "Date cannot be in the past!")]
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
         [Required]
         [Display(Name = "End Date")]
+        [CheckPastDate(ErrorMessage = "Date cannot be in the past!")]
+        [IsEndDateLessThanStartDate(otherPropertyName = "StartDate", ErrorMessage = "End date must be greater than start date")]
         public DateTime EndDate { get; set; }
         [Required]
         public string Description { get; set; }
